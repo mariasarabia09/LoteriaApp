248415728beb1827a9b2ffa42a20c5237a751c85
Maria Sarabia <mariasarabia09@yahoo.com>
2021-11-24 18:11:55 -0600

    Cleans the files to remove commented code and code with no references.

--- src/app/components/card-carousel/card-carousel.component.ts
+++ src/app/components/card-carousel/card-carousel.component.ts
@@ -4,7 +4,6 @@ import { Card } from 'src/app/models/card';
 import { CardService } from 'src/app/services/card.service';
 
 import * as $ from 'jquery';
-import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
     selector: 'card-carousel',
@@ -20,7 +19,6 @@ export class CardCarouselComponent {
     dealtCard: Card;
     gameStarted: Boolean = false;
     defaultCards: Card[];
-    lastFourCardsOfDeck: Boolean = false;
 
     ngOnInit() {
         this.cardService.cards$.subscribe(cards => {
@@ -33,8 +31,6 @@ export class CardCarouselComponent {
                 let options = $('#carousel-example').data();
                 options.interval = false;
             }
-
-            // console.log($('#carousel-example')[0].dataset.in);
 
             if (this.dealtCard) {
                 this.pastCards.push(this.dealtCard);
@@ -55,33 +51,5 @@ export class CardCarouselComponent {
 
         activeElement.removeClass("active");
         nextElement.addClass("active");
-    }
-
-        /*refillDefaultDeckOfCards(index: number): boolean{
-        if (index == 6)
-        {
-            console.log("there");
-
-            this.defaultCards.push({
-                cardId: 13,
-                cardNameSpanish: "El Gorrito",
-                cardNameEnglish: "The Bonnet",
-                imageUrl: "assets/images/13 - El Gorrito.jpg"
-            });
-        }
-
-        return true;
-
-        if(index + 4 >= this.defaultCards.length)
-        {
-            let variables = this.cardService.getDefaultCards();
-            variables.forEach((variable: Card) =>
-            {
-                this.defaultCards.push(variable);
-                console.log(this.defaultCards.length);
-            });
-        }
-        console.log("there");
-
-    }*/
+    };
 }
